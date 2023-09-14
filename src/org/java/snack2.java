ea6752846d54384835ecc777ecfabf1915a2efde
DanieleSpuria <123416873+DanieleSpuria@users.noreply.github.com>
2023-09-14 12:05:23 +0200

    snack2

--- src/org/java/snack2.java
+++ src/org/java/snack2.java
@@ -4,6 +4,21 @@ public class snack2 {
 
 	public static void main(String[] args) {
 			
+		String[] names = {
+				"Filippo", "Giovanni", "Gertrude", "Gianna"
+		};
+		
+		String[] lastnames = {
+				"Conservato", "Allaltezza", "Secondo", "Mulo"
+		};
+		
+		String[] invitati = new String[10];
+		
+		for (int i = 0; i < names.length; i++) {
+			
+			invitati[i] = names[i] + " " + lastnames[i];
+			System.out.println(invitati[i]);
+		}
 		
 	}
 }
